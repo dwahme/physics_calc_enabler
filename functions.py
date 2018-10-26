c3949797213a62e90702b7918beab783b91c856a
Dawson Whitehead <dwahme1@gmail.com>
2018-10-26 18:09:58 -0500

    Added uncertainty, and rotational force calculations

--- functions.py
+++ functions.py
@@ -1,3 +1,5 @@
+import math
+import statistics
 
 def get_average(values):
 
@@ -5,3 +7,19 @@ def get_average(values):
     amount = len(values)
 
     return total / amount
+
+def calc_uncertainty(values):
+    return statistics.stdev(values)
+
+def calc_rotational_force(mass, omega, radius):
+    return mass * omega * omega * radius
+
+def calc_rotational_force_uncertainty(mass, omega, radius, d_mass, d_omega, d_radius):
+    
+    mass_err = omega * omega * radius * d_mass
+    omega_err = d_omega * d_omega * radius * mass
+    radius_err = omega * omega * d_radius * mass
+
+    error_square_sum = mass_err * mass_err + omega_err * omega_err + radius_err * radius_err
+    
+    return math.sqrt(error_square_sum)
