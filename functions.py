0b0be560ad80345e95b8b750147ec4e36fd1656e
Dawson Whitehead <dwahme1@gmail.com>
2018-10-29 22:35:00 -0500

    Added more functions to function repository

--- functions.py
+++ functions.py
@@ -13,13 +13,28 @@ def calc_uncertainty(values):
 def percent_diff(expected, actual):
     return (expected - actual) / expected
 
+def calc_slope(x1, y1, x2, y2):
+    return (y2 - y1) / (x2 - x1)
+
+def calc_omega(time):
+    return 2 * math.pi / time
+
+def calc_omega_uncertainty(time, time_d):
+    omega_min = 2 * math.pi / (time + time_d)
+    omega_max = 2 * math.pi / (time - time_d)
+
+    return get_average([omega_min, omega_max])
+
+def calc_weight(mass):
+    return 9.8 * mass
+
 def calc_rotational_force(mass, omega, radius):
     return mass * omega * omega * radius
 
 def calc_rotational_force_uncertainty(mass, omega, radius, d_mass, d_omega, d_radius):
 
     mass_err = omega * omega * radius * d_mass
-    omega_err = d_omega * d_omega * radius * mass
+    omega_err = 2 * d_omega * omega * radius * mass
     radius_err = omega * omega * d_radius * mass
 
     error_square_sum = mass_err * mass_err + omega_err * omega_err + radius_err * radius_err
