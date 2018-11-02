9ca38dce2cd3c2d0bf83be056bd10a732e3a0a6f
Dawson Whitehead <dwahme1@gmail.com>
2018-11-02 18:18:59 -0500

    Added skeleton of calculation requirements and data inputs

--- 4_lab_explosions.py
+++ 4_lab_explosions.py
@@ -1,2 +1,75 @@
 import functions
 
+##################
+# BEGIN DATA INPUT
+##################
+
+mass_g1 = 0
+mass_g1_uncertainty = 0
+mass_g2 = 0
+mass_g2_uncertainty = 0
+
+# explosion measurements
+g1_explode_times = [
+    0, 0
+]
+g2_explode_times = [
+    0, 0
+]
+
+# elastic collision measurements
+flag_length = 0
+g1_initial_times = [
+    0, 0
+]
+g2_initial_times = [
+    0, 0
+]
+g1_final_times = [
+    0, 0
+]
+g2_final_times = [
+    0, 0
+]
+g1_initial_velocities = [
+    0, 0
+]
+g2_initial_velocities = [
+    0, 0
+]
+g1_final_velocities = [
+    0, 0
+]
+g2_final_velocities = [
+    0, 0
+]
+
+################
+# END DATA INPUT
+################
+
+
+# CALCULATIONS
+
+# get mean value of final velocity for g1
+# get mean value of final velocity for g2
+# get uncertainty of final velocity for g1
+# get uncertainty of final velocity for g2
+
+# calculate KE for g1 before explosion
+# calculate KE for g2 after explosion
+
+# if flag_length != 0, then need to calc velocities from times
+    # calculate g1 velocities after the elastic collision
+    # calculate g2 velocities after the elastic collision
+
+# calculate predicted velocity after the collision
+# compare prediction with measured
+
+# for each inelastic collision, calc KE for g1 before collis
+# for each inelastic collision, calc KE for g1 after collis
+# for each inelastic collision, calc KE for g2 before collis
+# for each inelastic collision, calc KE for g2 after collis
+
+# calc mean of KE's
+# calc spread of KE's
