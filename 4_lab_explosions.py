9fe9653561a5b0edaa988a6238fc3a21ce5cd16f
Dawson Whitehead <dwahme1@gmail.com>
2018-11-04 16:22:14 -0600

    Added elastic calculations

--- 4_lab_explosions.py
+++ 4_lab_explosions.py
@@ -10,7 +10,7 @@ mass_g2 = 1
 mass_g2_uncertainty = 0
 
 # explosion measurements
-explode_flag_length = 0
+flag_length = 0
 g1_explode_times = [
     0, 0
 ]
@@ -25,29 +25,48 @@ g2_explode_velocities = [
 ]
 
 # elastic collision measurements
-elastic_flag_length = 0
-g1_initial_times = [
+g1_elastic_times_0 = [
     0, 0
 ]
-g2_initial_times = [
+g1_elastic_times_1 = [
     0, 0
 ]
-g1_final_times = [
+g2_elastic_times_1 = [
     0, 0
 ]
-g2_final_times = [
+g1_elastic_vels_0 = [
     0, 0
 ]
-g1_initial_velocities = [
+g1_elastic_vels_1 = [
     0, 0
 ]
-g2_initial_velocities = [
+g2_elastic_vels_1 = [
     0, 0
 ]
-g1_final_velocities = [
+
+# inelastic collision measurements
+g1_inelastic_times_0 = [
     0, 0
 ]
-g2_final_velocities = [
+g2_inelastic_times_0 = [
+    0, 0
+]
+g1_inelastic_times_1 = [
+    0, 0
+]
+g2_inelastic_times_1 = [
+    0, 0
+]
+g1_inelastic_vels_0 = [
+    0, 0
+]
+g2_inelastic_vels_0 = [
+    0, 0
+]
+g1_inelastic_vels_1 = [
+    0, 0
+]
+g2_inelastic_vels_1 = [
     0, 0
 ]
 
@@ -58,14 +77,15 @@ g2_final_velocities = [
 
 # CALCULATIONS
 
+# Explosion calculations
 # predict the ratio of the velocities of the gliders using masses
 v_ratio = mass_g1 / mass_g2
 
 # Calculate velocites if necessary
-if explode_flag_length > 0:
-    g1_explode_velocities = [f.calc_velocity(explode_flag_length, time)
+if flag_length > 0:
+    g1_explode_velocities = [f.calc_velocity(flag_length, time)
         for time in g1_explode_times]
-    g2_explode_velocities = [f.calc_velocity(explode_flag_length, time)
+    g2_explode_velocities = [f.calc_velocity(flag_length, time)
         for time in g2_explode_times]
 
 # Get explosion velocities and uncertainties
@@ -78,20 +98,42 @@ g2_explode_vel_uncert = f.calc_uncertainty(g2_explode_velocities)
 explode_ke_0 = f.calc_ke(mass_g1, 0) + f.calc_ke(mass_g2, 0)
 explode_ke_1 = f.calc_ke(mass_g1, g1_explode_vel_ave) + f.calc_ke(mass_g2, g2_explode_vel_ave)
 
-# if flag_length != 0, then need to calc velocities from times
-    # calculate g1 velocities after the elastic collision
-    # calculate g2 velocities after the elastic collision
+
+# Elastic collision calclations
+# Calculate velocites if necessary
+if flag_length > 0:
+    g1_elastic_vels_0 = [f.calc_velocity(flag_length, time)
+        for time in g1_elastic_times_0]
+    g1_elastic_vels_1 = [f.calc_velocity(flag_length, time)
+        for time in g1_elastic_times_1]
+    g2_elastic_vels_1 = [f.calc_velocity(flag_length, time)
+        for time in g2_elastic_times_1]
+
+# Calculate momentum of gliders before and after collisions
+g1_momentums_0 = [f.calc_momentum(mass_g1, vel) 
+    for vel in g1_elastic_times_0]
+g2_momentums_0 = [0
+    for _ in g1_elastic_times_0]
+g1_momentums_1 = [f.calc_momentum(mass_g1, vel) 
+    for vel in g1_elastic_times_1]
+g2_momentums_1 = [f.calc_momentum(mass_g2, vel) 
+    for vel in g2_elastic_times_1]
+
+# Calculate total momentums before and after collisions
+elastic_momentum_tot_0 = [sum(x) for x in zip(g1_momentums_0, g2_momentums_0)]
+elastic_momentum_tot_1 = [sum(x) for x in zip(g1_momentums_1, g2_momentums_1)]
+
+# Calculate mean momentum and spread
+elastic_momentum_ave_0 = f.get_average(elastic_momentum_tot_0)
+elastic_momentum_ave_1 = f.get_average(elastic_momentum_tot_1)
+elastic_momentum_spread_0 = f.calc_uncertainty(elastic_momentum_tot_0) * 2
+elastic_momentum_spread_1 = f.calc_uncertainty(elastic_momentum_tot_1) * 2
 
 # calculate predicted velocity after the collision
-inelastic_velocities_g1 = [
-    0, 0
-]
-inelastic_velocities_g1_g2 = [
-    0, 0
-]
 #todo: dawson pls ^
 # compare prediction with measured
 
+# Inelastic collision calculations
 # for each inelastic collision, calc KE for g1 before collis
 inelastic_KE_g1_0 = []
 for col_vel in inelastic_velocities_g1:
