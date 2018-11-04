e315db614dbefb18781b01e7f8cda8fe76b0dbe1
Christina Ford <fordc@uchicago.edu>
2018-11-04 15:27:04 -0600

    added inelastic ke cals

--- 4_lab_explosions.py
+++ 4_lab_explosions.py
@@ -6,7 +6,7 @@ import functions as f
 
 mass_g1 = 0
 mass_g1_uncertainty = 0
-mass_g2 = 0
+mass_g2 = 1
 mass_g2_uncertainty = 0
 
 # explosion measurements
@@ -63,9 +63,9 @@ v_ratio = mass_g1 / mass_g2
 
 # Calculate velocites if necessary
 if explode_flag_length > 0:
-    g1_explode_velocities = [f.calc_velocity(explode_flag_length, time) 
+    g1_explode_velocities = [f.calc_velocity(explode_flag_length, time)
         for time in g1_explode_times]
-    g2_explode_velocities = [f.calc_velocity(explode_flag_length, time) 
+    g2_explode_velocities = [f.calc_velocity(explode_flag_length, time)
         for time in g2_explode_times]
 
 # Get explosion velocities and uncertainties
@@ -87,12 +87,33 @@ g2_explode_uncert = f.calc_uncertainty(g2_explode_velocities)
     # calculate g2 velocities after the elastic collision
 
 # calculate predicted velocity after the collision
+inelastic_velocities_g1 = [
+    0, 0
+]
+inelastic_velocities_g1_g2 = [
+    0, 0
+]
+#todo: dawson pls ^
 # compare prediction with measured
 
 # for each inelastic collision, calc KE for g1 before collis
+inelastic_KE_g1_0 = []
+for col_vel in inelastic_velocities_g1:
+    inelastic_KE_g1_0.append(f.calc_ke(mass_g1, col_vel))
 # for each inelastic collision, calc KE for g1 after collis
+inelastic_KE_g1_1 = []
+for col_vel in inelastic_velocities_g1_g2:
+    inelastic_KE_g1_1.append(f.calc_ke(mass_g1, col_vel))
 # for each inelastic collision, calc KE for g2 before collis
+inelastic_KE_g2_0 = [0 for _ in inelastic_velocities_g1]
 # for each inelastic collision, calc KE for g2 after collis
+inelastic_KE_g2_1 = []
+for col_vel in inelastic_velocities_g1_g2:
+    inelastic_KE_g2_1.append(f.calc_ke(mass_g2, col_vel))
 
+print(inelastic_KE_g1_0)
+print(inelastic_KE_g1_1)
+print(inelastic_KE_g2_0)
+print(inelastic_KE_g2_1)
 # calc mean of KE's
 # calc spread of KE's
