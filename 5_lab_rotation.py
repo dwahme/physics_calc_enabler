a984f9b0e09e917dedebb117de5f8668a05ba6ce
Dawson Whitehead <dwahme1@gmail.com>
2018-11-09 20:39:22 -0600

    Added calculations through question 5 for lab

--- 5_lab_rotation.py
+++ 5_lab_rotation.py
@@ -10,13 +10,29 @@ mass_m = 0
 radius_R = 0
 radius_r = 0
 
+mass_M_uncert = 0
+mass_m_uncert = 0
+radius_R_uncert = 0
+radius_r_uncert = 0
+
+fall_h = 0
+fall_uncert = 0
+
 ################
 # END DATA INPUT
 ################
 
 
 # Run calculations
+
+# Calculate basic theoretical values
 disk_moment = f.calc_moment_inertia_disk(mass_M, radius_R)
 tension_modifier = mass_m * radius_r * radius_r / disk_moment
 tension_approx = f.calc_weight(mass_m)
-tension_offset = 1 / (1 + tension_approx)
+tension_offset = 1 / (1 + tension_modifier)
+tension = tension_approx * tension_offset
+
+torque = tension * radius_r
+# mom_inertia = disk_moment
+
+alpha_0 = torque / disk_moment
