343f6e72a17d44cc6ec829e76e5915a954208191
Dawson Whitehead <dwahme1@gmail.com>
2018-11-16 18:18:11 -0600

    Added skeleton and variables for first few calculations

--- 6_lab_resonance.py
+++ 6_lab_resonance.py
@@ -5,7 +5,13 @@ import functions as f
 # BEGIN DATA INPUT
 ##################
 
+mass_g = 0
+mass_g_uncert = 0
+k_const = 0
 
+oscillation_periods = [
+    0, 0
+]
 
 ################
 # END DATA INPUT
@@ -14,6 +20,9 @@ import functions as f
 
 # CALCULATIONS
 
-
+# predict the period of oscillation using mass_g and k_const
+# get average oscillation period
+# get uncertainty of oscillation period using spread
+# determine if consistent? can be done manually
 
 # RESULTS
