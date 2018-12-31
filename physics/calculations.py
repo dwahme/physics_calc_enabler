d25bf2d83cc70a739d5f3bd77fab67d89e212aa5
Dawson Whitehead <dwahme1@gmail.com>
2018-12-31 01:08:23 -0600

    Added basic functions for beginning development of physics package

--- physics/calculations.py
+++ physics/calculations.py
@@ -2,3 +2,15 @@
 A submodule for running calculations for various values
 in both theoretical and experiemental physics.
 '''
+
+def velocity(distance, time):
+    # TODO: add documentation
+    pass
+
+def momentum(mass, velocity):
+    # TODO: add documentation
+    pass
+
+def energy_kinetic(mass, velocity):
+    # TODO: add documentation
+    pass
